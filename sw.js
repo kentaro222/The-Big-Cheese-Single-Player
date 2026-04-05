5efa07c9f680e0d13b579fa7e02de1809e2ec418
kentaro222 <magdracky@gmail.com>
2026-04-05 15:23:27 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,34 @@
+const CACHE_NAME = 'big-cheese-v1';
+const ASSETS_TO_CACHE = [
+  'index.html',
+  'manifest.json',
+  // ゲーム内で使用されている画像資産
+  'Card_2.png',
+  'Card_4.png',
+  'Card_6.png',
+  'Card_8.png',
+  'Card_10.png',
+  'Card_12.png',
+  'Card20.png',
+  'Card_Veto.png',
+  'Card_TheBigCheese.png',
+  'Mouse_sheet.png'
+];
+
+// インストール時にキャッシュ
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
+  );
+});
+
+// フェッチ時にキャッシュがあればそれを返し、なければネットワークから取得
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
